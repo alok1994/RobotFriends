43c389e3fbfb05d7a8437b92f516604dbd94b8b3
Kumar Verma <alok.kumar_verma@nokia.com>
2022-08-10 17:19:04 +0530

    fix package and errorboundry

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import Cardlist from "../components/Cardlist";
 import SearchBox from '../components/SearchBox'
 import './App.css'
 import Scroll from '../components/Scroll'
+import ErrorBoundry from "../components/ErrorBoundry";
 
 class App extends Component {
     constructor() {
@@ -35,7 +36,9 @@ class App extends Component {
                     <h1 className="f1">RobotFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
-                        <Cardlist robot={filterRobot}/> 
+                        <ErrorBoundry>
+                            <Cardlist robot={filterRobot}/> 
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
             );
