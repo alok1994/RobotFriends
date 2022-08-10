0fd26290def4a21a08c928ed7410633e6c97eadb
Kumar Verma <alok.kumar_verma@nokia.com>
2022-08-10 17:18:00 +0530

    Updates

---
+++ precache-manifest.aa0454e88139df6c8ef4e9086e91e838.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fff29da1be890cafc88",
+    "url": "/RobotFriends/static/css/main.c083da3f.chunk.css"
+  },
+  {
+    "revision": "6fff29da1be890cafc88",
+    "url": "/RobotFriends/static/js/main.6fff29da.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/RobotFriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "38b748206045b273faba",
+    "url": "/RobotFriends/static/css/2.93a9a533.chunk.css"
+  },
+  {
+    "revision": "38b748206045b273faba",
+    "url": "/RobotFriends/static/js/2.38b74820.chunk.js"
+  },
+  {
+    "revision": "7ecd2fd7ffcce96fe9cb",
+    "url": "/RobotFriends/static/js/runtime~main.7ecd2fd7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RobotFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "71e47ed5153b548bb9a11a0707175a53",
+    "url": "/RobotFriends/index.html"
+  }
+];
